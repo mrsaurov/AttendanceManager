76e26a9fdd6dd09c30192f981eb5dad0ad9b5033
Saurov <m.r.saurov@gmail.com>
2019-04-12 04:34:28 +0600

    Added section spinner while adding course, added stub layout for course info

--- app/src/main/java/com/saurov/attendancemanager/activities/AddEditCourseActivity.java
+++ app/src/main/java/com/saurov/attendancemanager/activities/AddEditCourseActivity.java
@@ -1,14 +1,15 @@
 package com.saurov.attendancemanager.activities;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
@@ -16,6 +17,17 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.orm.SugarRecord;
 import com.saurov.attendancemanager.R;
 import com.saurov.attendancemanager.database.Course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AddEditCourseActivity extends AppCompatActivity {
 
@@ -34,6 +46,9 @@ public class AddEditCourseActivity extends AppCompatActivity {
     @BindView(R.id.section_radio_group)
     RadioGroup sectionRadioGroup;
 
+    @BindView(R.id.section_spinner)
+    Spinner sectionSpinner;
+
     @BindView(R.id.save_button)
     MaterialButton saveButton;
 
@@ -43,6 +58,8 @@ public class AddEditCourseActivity extends AppCompatActivity {
     Course course;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,16 +67,20 @@ public class AddEditCourseActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        String editflag = getIntent().getStringExtra(CourseActivity.EDIT_COURSE_FLAG);
+        initializeSectionSpinner();
 
 
-        if (editflag != null) {
+        //////////
+
+        String editFlag = getIntent().getStringExtra(CourseActivity.EDIT_COURSE_FLAG);
+
+        if (editFlag != null) {
 
             //Edit Request
 
             long courseId = getIntent().getLongExtra(CourseActivity.COURSE_ID_TAG, 0);
 
-            course = Course.findById(Course.class, courseId);
+            course = SugarRecord.findById(Course.class, courseId);
 
             courseName.setText(course.getTitle());
             series.setText(course.getSeries());
@@ -137,6 +158,88 @@ public class AddEditCourseActivity extends AppCompatActivity {
     public void onBackPressed() {
 //        showCancelDialog();
         finish();
+    }
+
+    private void initializeSectionSpinner(){
+
+        String[] section = new String[]{
+                "Section",
+                "A Section",
+                "B Section",
+                "C Section"
+        };
+
+        final List<String> sectionList = new ArrayList<>(Arrays.asList(section));
+
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.spinner_item, sectionList) {
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                view.setPadding(0, view.getPaddingTop(), view.getPaddingRight(), view.getPaddingBottom());
+                return view;
+            }
+
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0) {
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+
+
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        sectionSpinner.setAdapter(spinnerArrayAdapter);
+
+        sectionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+
+                if (position != 0){
+
+                    TextView selectedText=  view.findViewById(R.id.spinner_text_view);
+                    selectedText.setTextColor(getResources().getColor(R.color.spinnerSelectedTextColor));
+                }
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if (position > 0) {
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     private void showCancelDialog() {
